fe2dd98e445ea2f3f1890c334ac797ffd6786cf6
moffatarap <arapaoa.moffat@gmail.com>
2016-03-27 12:19:55 +1300

    4.7 - navContentAdapt
    Nav bar content text now adapts to screensize.

--- Disaster Zone/style/javaScryptCustom.js
+++ Disaster Zone/style/javaScryptCustom.js
@@ -4,3 +4,15 @@ $(document).ready(function () {
    
 
 });
+
+$(window).resize(function () {
+    if (window.innerWidth < 480) {
+
+        $('.LatestNews').replaceWith('<h3><a href="">News Link</a></h3>');
+
+    } else if (window.innerWidth > 480) {
+
+        // Change back to original .LatestNews
+
+    }
+}).resize();
