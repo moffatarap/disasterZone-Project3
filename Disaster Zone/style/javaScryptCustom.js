462f381126c58e540ef4dcc170b59b12253aac78
moffatarap <arapaoa.moffat@gmail.com>
2016-03-27 15:00:44 +1300

    4.7 - break

--- Disaster Zone/style/javaScryptCustom.js
+++ Disaster Zone/style/javaScryptCustom.js
@@ -4,9 +4,19 @@ $(document).ready(function () {
     /**** NAV HIDE AND SHOW PHONE ****/
     var clickHamburger = true; //NAV ICON CLICK
 
-    $(".grid_1").click(function () {
+    /* CLICK VARABLE */
+    var clickHC; //Hamburger contain
+
+    $("#nav .grid_1").click(function () {
     //show nav expand div
-    $("#hamburgerContain").removeClass("hidden");
+        $("#hamburgerContain").removeClass("hidden");
+        $("#hamburgerContain").animate({ height: "auto" }); //sets animation height [FIX]
+
+        clickHamburger = false; //sets true to false
+        clickHC = clickHC + 1; //adds 1 to var
+
+        //Hide nav expand
+        
     
 
    
