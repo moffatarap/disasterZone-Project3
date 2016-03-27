546a624367e14808eabec5a0d0c0e6591630893a
moffatarap <arapaoa.moffat@gmail.com>
2016-03-27 14:13:51 +1300

    4.7 - weHaveBrokenTable
    Table on page 4 is broken, needs fixing.

--- Disaster Zone/style/javaScryptCustom.js
+++ Disaster Zone/style/javaScryptCustom.js
@@ -4,6 +4,14 @@ $(document).ready(function () {
     /**** NAV HIDE AND SHOW PHONE ****/
     var clickHamburger = true; //NAV ICON CLICK
 
+$(".grid_1").click(function () {
+    //show nav expand div
+    $("#hamburgerContain").removeClass("hidden");
+    
+
+   
+
+});
 
 });
 
