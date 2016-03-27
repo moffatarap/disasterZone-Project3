5030bf0246096c338b8b44b1abd50987b08a723b
moffatarap <arapaoa.moffat@gmail.com>
2016-03-27 14:17:48 +1300

    4.7 - basicNavDropDownWork
    Show of hidden div working, for the nav.

--- Disaster Zone/style/javaScryptCustom.js
+++ Disaster Zone/style/javaScryptCustom.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     /**** NAV HIDE AND SHOW PHONE ****/
     var clickHamburger = true; //NAV ICON CLICK
 
-$(".grid_1").click(function () {
+    $(".grid_1").click(function () {
     //show nav expand div
     $("#hamburgerContain").removeClass("hidden");
     
