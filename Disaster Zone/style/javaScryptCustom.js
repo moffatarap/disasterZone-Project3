decd50cda09ce0a300afc23b97cc87adad32fcee
moffatarap <arapaoa.moffat@gmail.com>
2016-03-27 13:20:08 +1300

    4.7 - timeForJS Hide and show.

--- Disaster Zone/style/javaScryptCustom.js
+++ Disaster Zone/style/javaScryptCustom.js
@@ -1,18 +1,9 @@
 /* custom javaScrypt MDDN352 P1 [2016] (300317288)*/
 
 $(document).ready(function () {
+    /**** NAV HIDE AND SHOW PHONE ****/
+    var clickHamburger = true; //NAV ICON CLICK
 
 
 });
 
-$(window).resize(function () {
-    if (window.innerWidth < 480) {
-
-        $('.LatestNews').replaceWith('<h3><a href="">News Link</a></h3>');
-
-    } else if (window.innerWidth > 480) {
-
-        // Change back to original .LatestNews
-
-    }
-}).resize();
