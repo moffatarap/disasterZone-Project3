c114f4208eec06c30e255a50943eee94864ae4c6
moffatarap <arapaoa.moffat@gmail.com>
2016-03-28 10:01:29 +1300

    5.1 - startingPoint
    Starting point for week 5

--- Disaster Zone/style/javaScryptCustom.js
+++ Disaster Zone/style/javaScryptCustom.js
@@ -5,23 +5,28 @@ $(document).ready(function () {
     var clickHamburger = true; //NAV ICON CLICK
 
     /* CLICK VARABLE */
-    var clickHC; //Hamburger contain
+    var clickHC = 0; //Hamburger contain
 
     $("#nav .grid_1").click(function () {
     //show nav expand div
         $("#hamburgerContain").removeClass("hidden");
-        $("#hamburgerContain").animate({ height: "auto" }); //sets animation height [FIX]
+       
 
         clickHamburger = false; //sets true to false
         clickHC = clickHC + 1; //adds 1 to var
 
         //Hide nav expand
-        
-    
+        if (clickHC == 2) {
+            $("#hamburgerContain").addClass("hidden");
+            
+            clickHamburger = true; //sets click yes to true
+            clickHC = 0; //resets value
+        }  
 
-   
+    });
 
-});
+
+
 
 });
 
