83c53cffa0a2c93bb4c0ae87c5e64f84b69834f4
moffatarap <arapaoa.moffat@gmail.com>
2016-04-29 16:36:52 +1200

    9.5 - pushBeforeEdits
    Layout is kinda gross atm :(

--- Disaster Zone/style/geoLocationAPI.js
+++ Disaster Zone/style/geoLocationAPI.js
@@ -456,42 +456,42 @@ setInterval(function () {
 
         //0# - EARTHQUAKE PAEKAKARIKI || SEVERE
         if (latitude <= disasterLocLatArray[0] + disasterOffsetArray[0] && latitude >= disasterLocLatArray[0] - disasterOffsetArray[0] && longitude <= disasterLocLngArray[0] + disasterOffsetArray[1] && longitude >= disasterLocLngArray[0] - disasterOffsetArray[1]) {
-            document.getElementById("disasterAlert0").innerHTML = "<p>Warning: EARTHQUAKE</p>" + "<br/>";
+            document.getElementById("disasterAlert0").innerHTML = "<p>Warning: EARTHQUAKE</p>";
             $("#disasterAlert0").css({ "visibility": "visible" }); //makes div visible
             console.log('ALERT: Earthquake'); //debug
         };
 
         //1# - FIRE PAEKAKARIKI || MODERATE
         if  (latitude <= disasterLocLatArray[1] + disasterOffsetArray[4] && latitude >= disasterLocLatArray[1] - disasterOffsetArray[4] && longitude <= disasterLocLngArray[1] + disasterOffsetArray[5] && longitude >= disasterLocLngArray[1] - disasterOffsetArray[5]) {
-            document.getElementById("disasterAlert1").innerHTML = "<p>Warning: FIRE</p>" + "<br/>";
+            document.getElementById("disasterAlert1").innerHTML = "<p>Warning: FIRE</p>";
             $("#disasterAlert1").css({ "visibility": "visible" }); //makes div visible
             console.log('ALERT: FIRE'); //debug
         };
 
         //2# - FLOOD WELLINGTON || LIGHT
         if (latitude <= disasterLocLatArray[2] + disasterOffsetArray[6] && latitude >= disasterLocLatArray[2] - disasterOffsetArray[6] && longitude <= disasterLocLngArray[2] + disasterOffsetArray[7] && longitude >= disasterLocLngArray[2] - disasterOffsetArray[7]) {
-            document.getElementById("disasterAlert2").innerHTML = "<p>Warning: FLOOD</p>" + "<br/>";
+            document.getElementById("disasterAlert2").innerHTML = "<p>Warning: FLOOD</p>";
             $("#disasterAlert2").css({ "visibility": "visible" }); //makes div visible
             console.log('ALERT: FLOOD'); //debug
         };
 
         //3# - HURRICANE WELLINGTON || STRONG
         if (latitude <= disasterLocLatArray[3] + disasterOffsetArray[2] && latitude >= disasterLocLatArray[3] - disasterOffsetArray[2] && longitude <= disasterLocLngArray[3] + disasterOffsetArray[3] && longitude >= disasterLocLngArray[3] - disasterOffsetArray[3]) {
-            document.getElementById("disasterAlert3").innerHTML = "<p>Warning: HURRICANE</p>" + "<br/>";
+            document.getElementById("disasterAlert3").innerHTML = "<p>Warning: HURRICANE</p>";
             $("#disasterAlert3").css({ "visibility": "visible" }); //makes div visible
             console.log('ALERT: HURRICANE'); //debug
         };
 
         //4# - TORNADO WELLINGTON || WEAK
         if (latitude <= disasterLocLatArray[4] + disasterOffsetArray[8] && latitude >= disasterLocLatArray[4] - disasterOffsetArray[8] && longitude <= disasterLocLngArray[4] + disasterOffsetArray[9] && longitude >= disasterLocLngArray[4] - disasterOffsetArray[9]) {
-            document.getElementById("disasterAlert4").innerHTML = "<p>Warning: TORNADO</p>" + "<br/>";
+            document.getElementById("disasterAlert4").innerHTML = "<p>Warning: TORNADO</p>";
             $("#disasterAlert4").css({ "visibility": "visible" }); //makes div visible
             console.log('ALERT: TORNADO'); //debug
         };
 
         //5# - FIRE WELLINGTON TE ARO || WEAK
         if (latitude <= disasterLocLatArray[5] + disasterOffsetArray[6] && latitude >= disasterLocLatArray[5] - disasterOffsetArray[6] && longitude <= disasterLocLngArray[5] + disasterOffsetArray[7] && longitude >= disasterLocLngArray[5] - disasterOffsetArray[7]) {
-            document.getElementById("disasterAlert5").innerHTML = "<p>Warning: FIRE</p>" + "<br/>";
+            document.getElementById("disasterAlert5").innerHTML = "<p>Warning: FIRE</p>";
             $("#disasterAlert5").css({ "visibility": "visible" }); //makes div visible
             console.log('ALERT: FIRE'); //debug
         };
