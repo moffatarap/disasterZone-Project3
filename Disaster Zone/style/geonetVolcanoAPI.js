bb44d1730a5ab5147eccd81ad96a885de2525257
moffatarap <arapaoa.moffat@gmail.com>
2016-05-15 13:02:30 +1200

    11.7 - testPageJson
    Getting realtime data from Geonet on volcanos, currently works local
    only working on getting it live from geonet.

--- Disaster Zone/style/geonetVolcanoAPI.js
+++ Disaster Zone/style/geonetVolcanoAPI.js
@@ -2,47 +2,6 @@
 /*=/ VARABLES \=*/
 
 /*== ARRAYS ==*/
-/*
-window.onload = function () {
-$(function(){
-    $.getJSON ('./json/geoNetVolcanoVal.json',function(data){
-        console.log("Volcano Info");
-        $('#severityScale').append(data.properties[0].volcanoID);
-        
-    })
-}
-
-)}
-*/
-//window.onload = function() {
-//var json = {
-//    "features": [{
-//        "type": "Feature",
-//        "geometry": {
-//            "type": "Point",
-//            "coordinates": [174.77, -36.985]
-//        },
-//        "properties": {
-//            "volcanoID": "aucklandvolcanicfield",
-//            "volcanoTitle": "Auckland Volcanic Field",
-//            "level": 0,
-//            "activity": "No volcanic unrest.",
-//            "hazards": "Volcanic environment hazards."
-//        }
-//   }]
-//}
-
-//for (var i = 0; i < json.features.length; i++) {
-//    var obj = json.properties[i];
-//    if (obj.category == 1) {
-//        $("#alertscontain").append("<tr><td>" + obj.volcanoID + " " + obj.volcanoTitle + "</td><td></td></tr>");
-//    }
-//    else if (obj.category == 2) {
-//        $("#alertscontain").append("<tr><td></td><td>" + obj.volcanoID + " " + obj.volcanoTitle + "</td></tr>");
-//    }
-//}
-
-//}
 
 $(function () {
     //gets JSON from defined locaiton [LOCAL]
