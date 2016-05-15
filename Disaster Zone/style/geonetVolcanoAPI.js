c0bfb4696509a76de3c2971f4f9287aafcc4fd77
moffatarap <arapaoa.moffat@gmail.com>
2016-05-15 12:13:02 +1200

    11.7 - readingDataFromJson
    Reading data from JSON for volcanos does not work online just yet.

--- Disaster Zone/style/geonetVolcanoAPI.js
+++ Disaster Zone/style/geonetVolcanoAPI.js
@@ -14,32 +14,47 @@ $(function(){
 
 )}
 */
-window.onload = function() {
-var json = {
-    "features": [{
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [174.77, -36.985]
-        },
-        "properties": {
-            "volcanoID": "aucklandvolcanicfield",
-            "volcanoTitle": "Auckland Volcanic Field",
-            "level": 0,
-            "activity": "No volcanic unrest.",
-            "hazards": "Volcanic environment hazards."
-        }
-   }]
-}
+//window.onload = function() {
+//var json = {
+//    "features": [{
+//        "type": "Feature",
+//        "geometry": {
+//            "type": "Point",
+//            "coordinates": [174.77, -36.985]
+//        },
+//        "properties": {
+//            "volcanoID": "aucklandvolcanicfield",
+//            "volcanoTitle": "Auckland Volcanic Field",
+//            "level": 0,
+//            "activity": "No volcanic unrest.",
+//            "hazards": "Volcanic environment hazards."
+//        }
+//   }]
+//}
 
-for (var i = 0; i < json.features.length; i++) {
-    var obj = json.properties[i];
-    if (obj.category == 1) {
-        $("#alertscontain").append("<tr><td>" + obj.volcanoID + " " + obj.volcanoTitle + "</td><td></td></tr>");
-    }
-    else if (obj.category == 2) {
-        $("#alertscontain").append("<tr><td></td><td>" + obj.volcanoID + " " + obj.volcanoTitle + "</td></tr>");
-    }
-}
+//for (var i = 0; i < json.features.length; i++) {
+//    var obj = json.properties[i];
+//    if (obj.category == 1) {
+//        $("#alertscontain").append("<tr><td>" + obj.volcanoID + " " + obj.volcanoTitle + "</td><td></td></tr>");
+//    }
+//    else if (obj.category == 2) {
+//        $("#alertscontain").append("<tr><td></td><td>" + obj.volcanoID + " " + obj.volcanoTitle + "</td></tr>");
+//    }
+//}
 
-}
+//}
+
+$(function () {
+    //gets JSON from defined locaiton [LOCAL]
+    $.getJSON('./json/geoNetVolcanoVal.json', function (data) {
+        //each data features array is looped for its length
+        $.each(data.features, function (i, f) {
+            //data id displayed in table row || this one is volcano title
+            var tblRow = "<tr>" + "<td>" + f.properties.volcanoTitle + "</td>" + 
+             "<td>" + f.properties.level + "</td>" + "<td>" + f.properties.activity + "</td>" + "<td>" + f.properties.hazards + "</td>" + "</tr>"
+            $(tblRow).appendTo("#userdata tbody"); //appends all user data to userdata id under tbody
+        });
+
+    });
+
+});
