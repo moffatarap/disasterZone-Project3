405154b9c48408d79d1b7c85d1c822c5ecefeab7
moffatarap <arapaoa.moffat@gmail.com>
2016-05-23 10:05:14 +1200

    13.1 - addingVarablesForEarthQuake
    Adding Vairables for earthquakes.

--- Disaster Zone/style/geonetEarthquakeAPI.js
+++ Disaster Zone/style/geonetEarthquakeAPI.js
@@ -1,9 +1,25 @@
 /* Geonet Earthquake API Disaster Zone MDDN352 P3 13.1 - [2016] MOFFATARAP (300317288) */
 /*=/ VARABLES \=*/
 var geonetEarthQuake = "https://json.ey.nz/www.geonet.org.nz/quakes/services/felt.json"; //saves goenet url as var
-
+var earthQEventLength = 8; //sets earthquake array max events
+var earthQRadiusMulti = 2; //sets earthquake alert radius to be multiplyed by
 /*== ARRAYS ==*/
 
+/* 1# EARTHQUAKE VARABLE ARRAYS */
+//Earthquake Intensity || Light,Weak,Moderate,Strong,Severe
+var earthQIntesityArray = [
+];
+
+//Earthquake Magitude
+var earthQMagnitudeArray = [
+];
+
+//Earthquake coordinates || LAT LNG
+var earthQLatLngArray = [
+];
+
+
+/* 1# EARTHQUAKE VARABLE ARRAYS [END]*/
 function earthJSON() {
     //gets JSON from defined locaiton [LOCAL '/json/geoNetEarthquakeVal.json']
     $.getJSON(geonetEarthQuake, function (data) {
