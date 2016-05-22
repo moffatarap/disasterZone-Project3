68b144aac7a581acd2d781d274ca6bc43a17b478
moffatarap <arapaoa.moffat@gmail.com>
2016-05-23 09:54:46 +1200

    13.1 - labStart
    Starting in lab beginning earthquake scrypt.

--- Disaster Zone/style/geonetEarthquakeAPI.js
+++ Disaster Zone/style/geonetEarthquakeAPI.js
@@ -1,13 +1,12 @@
-/* Geonet Earthquake API Disaster Zone MDDN352 P3 [2016] MOFFATARAP (300317288) */
+/* Geonet Earthquake API Disaster Zone MDDN352 P3 13.1 - [2016] MOFFATARAP (300317288) */
 /*=/ VARABLES \=*/
 var geonetEarthQuake = "https://json.ey.nz/www.geonet.org.nz/quakes/services/felt.json"; //saves goenet url as var
 
 /*== ARRAYS ==*/
 
-$(function () {
+function earthJSON() {
     //gets JSON from defined locaiton [LOCAL '/json/geoNetEarthquakeVal.json']
     $.getJSON(geonetEarthQuake, function (data) {
-
         //each data features array is looped for its length
         $.each(data.features, function (i, eq) {
             //data id displayed in table row
