752b8c05e2fd50b8df7bc4e04c6a51e53199c14e
moffatarap <arapaoa.moffat@gmail.com>
2016-05-23 10:57:27 +1200

    13.1 - changesAndUpdates
    Earthquake json access to var works.

--- Disaster Zone/style/geonetEarthquakeAPI.js
+++ Disaster Zone/style/geonetEarthquakeAPI.js
@@ -1,6 +1,7 @@
 /* Geonet Earthquake API Disaster Zone MDDN352 P3 13.1 - [2016] MOFFATARAP (300317288) */
 /*=/ VARABLES \=*/
 var geonetEarthQuake = "https://json.ey.nz/www.geonet.org.nz/quakes/services/felt.json"; //saves goenet url as var
+var geonetEarthQuakeLocal = "./json/geoNetEarthquakeVal.json"
 var earthQEventLength = 8; //sets earthquake array max events
 var earthQRadiusMulti = 2; //sets earthquake alert radius to be multiplyed by
 /*== ARRAYS ==*/
@@ -18,21 +19,42 @@ var earthQMagnitudeArray = [
 var earthQLatLngArray = [
 ];
 
-
+//Earthquake ID || Geonets name for the event
+var earthQIDNameArray = [
+];
+window.onload = function () {
+    earthJSON();
+};
 /* 1# EARTHQUAKE VARABLE ARRAYS [END]*/
 function earthJSON() {
-    //gets JSON from defined locaiton [LOCAL '/json/geoNetEarthquakeVal.json']
-    $.getJSON(geonetEarthQuake, function (data) {
-        //each data features array is looped for its length
-        $.each(data.features, function (i, eq) {
-            //data id displayed in table row
-            var tblRow = "<tr>" + "<td>" + eq.properties.intensity + "</td>" +
-             "<td>" + eq.properties.magnitude + "</td>" + "<td>" + eq.geometry.coordinates + "</td>" + "<td>" + eq.id + "</td>" + "</tr>"
-            $(tblRow).appendTo("#userdata2 tbody"); //appends all user data to userdata id under tbody
+    $.getJSON(geonetEarthQuakeLocal, function (data) {
+        $.each(data.features, function (i2, eq) {
+            //data id displayed in table row || this one is volcano title
+            if (i2 < earthQEventLength) {
+                earthQIntesityArray[i2] = eq.properties.intensity;
+                earthQMagnitudeArray[i2] = eq.properties.magnitude;
+                earthQLatLngArray[i2] = eq.geometry.coordinates;
+                earthQIDNameArray[i2] = eq.id;
+                i2++;
+            }
+            else {
+                //doNothing
+            }
 
-           
         });
 
+        /* DEBUGGING */
+        console.log('#1 Intensity');
+        console.log(earthQIntesityArray); //display value of title array
+        console.log('#2 Magitude');
+        console.log(earthQMagnitudeArray); //display value of level array
+        console.log('#3');
+        console.log(earthQLatLngArray); //display value of activity array
+        console.log('#4');
+        console.log(earthQIDNameArray); //display value of hazard array
+        //[DEBUG DISPLAY]document.getElementById("errorCantFind").innerHTML = volcanoLevelArray[11];
+
+        //volcanoMarkerCreateLoop(); //calls vol marker loop
     });
-  
-});
+
+}
